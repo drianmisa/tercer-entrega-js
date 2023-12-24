77d5073d2e0f1baa48c269110be94366993c765c
Adrian Islas <adrian.islas.lopez@gmail.com>
2023-12-23 21:12:29 -0600

    Segundo guardad

---
+++ assets/js/main.js
@@ -0,0 +1,6 @@
+let contenedor = document.querySelector(".contenedor_main");
+let botonInicio =document.querySelector("#btn-inicio");
+function iniciarPrograma(event){
+    event.preventDefault();
+}
+botonInicio.addEventListener('click', iniciarPrograma);
